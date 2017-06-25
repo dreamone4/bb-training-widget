d5d239df090301eac2030d7715721a662ec05380
Roberto R <dreamone4@gmail.com>
2017-06-25 13:57:04 +0200

    Completed assignment

--- js/accounts.js
+++ js/accounts.js
@@ -3,16 +3,25 @@
 function initAccounts(widget) {
     
     // 1. Get widget preferences
-    var widgetTitle;
-    var dataSource;
+    var widgetTitle = widget.getPreference('title');
+    var dataSource = widget.getPreference('dataSource');
+    dataSource = dataSource.replace('$(contextRoot)/', b$.portal.portalServer.serverURL);
 
     // 2. Render widget title
     var $widget = $(widget.body);
+    $widget.find('.widgetTitle').html(widgetTitle);
     
     // 3. Retrieve accounts data
+    $.getJSON(dataSource, renderList);
 
     // 4. Render list of accounts
     function renderList(data) {
+        
+        var html = '';
+        $.each(data.accounts, function(index, account) {
+            html += '<li class="list-group-item">' + account.name + ' - EUR ' + account.balance + '</li>';
+        });
 
+        $widget.find('.accountsList').html(html);
     }
 }
