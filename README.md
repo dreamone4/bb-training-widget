ad7e011b8d5c6ca6e8ae1288594c80d46e8c6dc6
Roberto R <dreamone4@gmail.com>
2017-06-25 13:52:23 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# bb-training-widget
-Backbase Training - Widget assignment
---
+++ js/accounts.js
@@ -0,0 +1,18 @@
+
+
+function initAccounts(widget) {
+    
+    // 1. Get widget preferences
+    var widgetTitle;
+    var dataSource;
+
+    // 2. Render widget title
+    var $widget = $(widget.body);
+    
+    // 3. Retrieve accounts data
+
+    // 4. Render list of accounts
+    function renderList(data) {
+
+    }
+}
